c64777f24a76cc7c3e01985ea7037e390e3490b7
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-08-17 15:47:13 -0300

    Add files via upload

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,39 +1,40 @@
 import React from 'react'
 import './index.css'
+import Noticias from '../Noticias'
 
 function Home() {
     return (
-        <div className="divMainContainer">
-            <p>Home!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Home!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Home!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Home!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Home!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Home!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
-            <p>Ola Mundo!</p>
+        <div className="divMainContainerHome">
+            <div className="divVideoMainContainerHome">
+                <div className="divVideoShadowMainContainerHome" />
+                <video width="100%" autoPlay muted loop >
+                    <source src={require("./assets/viideo.webm")} type="video/webm"/>
+                </video>
+                
+            </div>
+            <Noticias/>
+            <div>
+
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+                <p>Olá mundo!</p>
+            </div>
         </div>
         )
 }
