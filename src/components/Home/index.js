017bc13fcd70535e5baa568abed408fc6b929fcf
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-08-18 17:33:57 -0300

    Add files via upload

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,14 +3,52 @@ import './index.css'
 import Noticias from '../Noticias'
 
 function Home() {
+    let dotVideo = 1
+    let video1 = true
+    let video2 = false
+    let video3 = false
+    const videoCarrossel = () => {
+        console.log(dotVideo)
+        dotVideo++
+        console.log(dotVideo)
+        if (dotVideo == 4) {
+            dotVideo = 1
+        }
+        switch (dotVideo) {
+            case 1:
+
+                break
+            case 2:
+                break
+            case 3:
+                break
+        }
+    }
+
+
     return (
         <div className="divMainContainerHome">
-            <div className="divVideoMainContainerHome">
-                <div className="divVideoShadowMainContainerHome" />
-                <video width="100%" autoPlay muted loop >
-                    <source src={require("./assets/viideo.webm")} type="video/webm"/>
-                </video>
-                
+            <div className="divCarrosselMainContainerHome">
+                <div className="divVideoMainContainerHome">
+                    <div className="divVideoShadowMainContainerHome" />
+                    <div className="divLabelCarrosselMainContainerHome">
+                        <h1 className="h1DivSecond">Título</h1>
+                        <label>O texto informativo é um texto em que o escritor expõe brevemente um tema</label>
+                    </div>
+                    <video width="100%" autoPlay muted loop onEnded={() => videoCarrossel()}>
+                        <source src={require("./assets/viideo.webm")} type="video/webm"/>
+                    </video>
+                </div>
+                <div className="divDots">
+                    <div>
+                        <input checked={video1} type="radio" id="rad1" name="rads"/>
+                        <label></label>
+                        <input checked={video2} type="radio" id="rad2" name="rads" />
+                        <label></label>
+                        <input checked={video3} type="radio" id="rad3" name="rads" />
+                        <label></label>
+                    </div>
+                </div>
             </div>
             <Noticias/>
             <div className='divSecondMainContainerHome'>
