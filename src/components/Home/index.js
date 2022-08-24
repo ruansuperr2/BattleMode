eb730c2f584a8ec9d4c8ff9f7c3f86a8fa7c9a11
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-08-24 16:27:54 -0300

    Add files via upload

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,62 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css'
 import Noticias from '../Noticias'
 
 function Home() {
-    let dotImage = 1
 
     const [imageDotOne, setImageDotOne] = useState(true)
     const [imageDotTwo, setImageDotTwo] = useState(false)
     const [imageDotThree, setImageDotThree] = useState(false)
     const [carrosselImage, setCarrosselImage] = useState('./assets/CSGO.png')
-    
-    function callbackTimeout() {
-        setTimeout(() => {
-            dotImage++
-            if(dotImage === 4){
-                dotImage = 1
-            }
 
-            switch(dotImage){
-                case 1:
-                    setCarrosselImage('./assets/CSGO.png')
-                    setImageDotOne(true)
-                    setImageDotTwo(false)
-                    setImageDotThree(false)
-                    setTimeout(() => {
-                        callbackTimeout() 
-                    }, 5000);
-                   
-                    break
-                case 2:
-                    setCarrosselImage('./assets/F1_2022.png')
-                    setImageDotOne(false)
-                    setImageDotTwo(true)
-                    setImageDotThree(false)
-                    setTimeout(() => {
-                        callbackTimeout() 
-                    }, 5000);
-                    break
-                case 3:
-                    setCarrosselImage('./assets/R6S.png')
-                    setImageDotOne(false)
-                    setImageDotTwo(false)
-                    setImageDotThree(true)
-                    setTimeout(() => {
-                        callbackTimeout() 
-                    }, 5000);
-                    break
-                default:
-                    break
-            }
+    console.log(imageDotOne, imageDotTwo, imageDotThree, carrosselImage)
+    setTimeout(() => {
+        if (imageDotOne === false && imageDotThree === true && imageDotTwo === false) {
+            setCarrosselImage('./assets/CSGO.png')
+            setImageDotOne(true)
+            setImageDotTwo(false)
+            setImageDotThree(false);
 
-        }, 5000)
-    }
+        } else if (imageDotOne === true && imageDotThree === false && imageDotTwo === false) {
+            setCarrosselImage('./assets/F1_2022.png')
+            setImageDotOne(false)
+            setImageDotTwo(true)
+            setImageDotThree(false);
 
-    
+        } else if (imageDotOne === false && imageDotThree === false && imageDotTwo === true) {
+            setCarrosselImage('./assets/R6S.png')
+            setImageDotOne(false)
+            setImageDotTwo(false)
+            setImageDotThree(true);
+
+        }
+    }, 2000)
+
+
+
+
 
     return (
-        <div className="divMainContainerHome" onLoad={callbackTimeout()}>
+        <div className="divMainContainerHome">
             <div className="divCarrosselMainContainerHome">
                 <div className="divVideoMainContainerHome">
                     <div className="divVideoShadowMainContainerHome" />
@@ -64,7 +45,7 @@ function Home() {
                         <h1 className="h1DivSecond">Título</h1>
                         <label>O texto informativo é um texto em que o escritor expõe brevemente um tema</label>
                     </div>
-                <img className="imgCarrossel" src={require(`${carrosselImage}`)}/>
+                    <img className="imgCarrossel" src={require(`${carrosselImage}`)} />
                 </div>
                 <div className="divDots">
                     <div>
