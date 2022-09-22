9c514e13458cab51f542a5742a0aa92f601623f6
mathêe <91820113+notmathee@users.noreply.github.com>
2022-09-22 15:11:29 -0300

    Add files via upload

--- src/components/Feed/components/Prediletos/Torneio/index.js
+++ src/components/Feed/components/Prediletos/Torneio/index.js
@@ -4,9 +4,9 @@ import placeholderArt from './arteOnda2.png'
 
 function Torneio() {
     return (
-        <div className="Torneio" style={{backgroundImage: `url(${placeholderArt})`}}>
+        <a href='./participar' className='Torneio' style={{backgroundImage: `url(${placeholderArt})`}}>
             <h1 className='TorneioH1'>Gaia Cup</h1>
-        </div>
+        </a>
         )
 }
 
