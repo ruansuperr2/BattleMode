6e125ae5e83c896316240c33bab3e804ae217709
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-10-03 16:00:32 -0300

    Add files via upload

--- src/components/Usuario/index.js
+++ src/components/Usuario/index.js
@@ -1,28 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
-import Fundo from './components/Fundo'
-import JogosFav from './components/JogosFav'
-import Descricao from './components/Descricao'
-import Conquistas from './components/Conquistas'
 import Navbar from '../Navbar'
 import Footer from '../Footer'
 import { useParams } from 'react-router-dom';
 
+let oneResponseONLY = 0
 function Usuario(){
     const { id } = useParams();
     console.log(id)
+
+    const [loggedUser, setLoggedUser] = useState({}) 
+    const [users, setUsers] = useState([])
+    const getUsers = async () => {
+        try{
+          const response = await fetch('http://localhost:3000/api/user')
+          const data = response.json()
+          console.log(data)
+          if(oneResponseONLY === 0){
+            data.then(
+                (val) => {
+                  setUsers(val.data)
+                  setLoggedUser(val.data.find((account) => {return account.username === id }))
+                  oneResponseONLY = 1
+                }
+                  
+            )
+            
+          }
+
+          console.log(data, data.val)
+        }catch(error){
+          console.log(error)
+        }
+    }
+    getUsers()
+    
+    console.log(loggedUser,'logged user')
+
+
     return(
         
         <div className="divUsuarioDMainContainer">
             <Navbar page={'usuario'}/>
 
                 <div className='divUsuarioSubMainContainer paddingLeft '>
-                    <button>Editar</button>
-                    <Fundo id={id}/>
+
+                    <div className='divFundoMainContainer'>
+                        <div className='divContainerFundoMainContainer'/>
+                    </div>
+                        <div className='perfilConfigGear'><div className='imgUsuarioGearEditing'/>Configurar Perfil</div>
                     <div className='divUsuarioSubMainContainerCompo' >
-                        <Descricao id={id}/>
-                        <JogosFav id={id}/>
-                        <Conquistas id={id}/>
+                    <h1 className="UserNameOnProfile"><div className='divImgFundoMainContainer' style={{backgroundImage: `url(${loggedUser.icon})`}}/>{loggedUser.username}</h1>
+                    
                     </div>
                 </div>
 
