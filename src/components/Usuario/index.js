adaeaa59dbbe2f14d37ea013517891f9124999db
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-10-04 17:12:37 -0300

    Add files via upload

--- src/components/Usuario/index.js
+++ src/components/Usuario/index.js
@@ -5,34 +5,48 @@ import Footer from '../Footer'
 import { useParams } from 'react-router-dom';
 
 let oneResponseONLY = 0
+let times
 function Usuario(){
     const { id } = useParams();
     console.log(id)
 
     const [loggedUser, setLoggedUser] = useState({}) 
+    const [currentTeams, setCurrentTeams] = useState({})
     const [users, setUsers] = useState([])
+    const [teams, setTeams] = useState({})
+
+    console.log('wfwefiosvzp]kosvvff', teams)
     const getUsers = async () => {
         try{
-          const response = await fetch('http://localhost:3000/api/user')
-          const data = response.json()
-          console.log(data)
-          if(oneResponseONLY === 0){
-            data.then(
-                (val) => {
-                  setUsers(val.data)
-                  setLoggedUser(val.data.find((account) => {return account.username === id }))
-                  document.querySelector('.divEquipesSubMainContainerCompo').style.display = 'none'
-                  document.querySelector('.divTorneiosSubMainContainerCompo').style.display = 'none'
-                  document.querySelector('.divConfigSubMainContainerCompo').style.display = 'none'
-                  document.querySelector('.geral').classList.add('perfilActive')
-                  oneResponseONLY = 1
-                }
-                  
-            )
-            
-          }
-
-          console.log(data, data.val)
+            const responseTeam = await fetch('http://localhost:3000/api/time')
+            const response = await fetch('http://localhost:3000/api/user')
+            const data = response.json()
+            const dataTeam = responseTeam.json()
+            console.log(data)
+            if(oneResponseONLY === 0){
+                dataTeam.then(
+                    (val) => {
+                        console.log(times, 'cavalo')
+                        times = val.data.find((team) => {return team.id === 1 })
+                        setTeams(times);
+                        console.log(teams, 'essacaceta')
+                    }
+                    )
+                data.then(
+                    (val) => {
+                        setUsers(val.data)
+                        setLoggedUser(val.data.find((account) => {return account.username === id }))
+                        document.querySelector('.divEquipesSubMainContainerCompo').style.display = 'none'
+                        document.querySelector('.divTorneiosSubMainContainerCompo').style.display = 'none'
+                        document.querySelector('.divConfigSubMainContainerCompo').style.display = 'none'
+                        document.querySelector('.geral').classList.add('perfilActive')
+                        oneResponseONLY = 1
+                        
+                    }
+                        
+                )
+                
+            }
         }catch(error){
           console.log(error)
         }
@@ -87,7 +101,7 @@ function Usuario(){
                 break
         }
     }
- 
+    
 
     return(
         
@@ -110,8 +124,7 @@ function Usuario(){
                     
                     </div>
                     <div className='divEquipesSubMainContainerCompo' >
-                        <h1 className="UserNameOnProfile"><div className='divImgFundoMainContainer' style={{backgroundImage: `url(${loggedUser.icon})`}}/>{loggedUser.username}2</h1>
-                    
+                        <h1 className="TeamsOnProfile"><div className='divImgFundoMainContainer' style={{backgroundImage: `url(${loggedUser.icon})`}}/>{teams.nome}2</h1>
                     </div>
                     <div className='divTorneiosSubMainContainerCompo' >
                         <h1 className="UserNameOnProfile"><div className='divImgFundoMainContainer' style={{backgroundImage: `url(${loggedUser.icon})`}}/>{loggedUser.username}3</h1>
