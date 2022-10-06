fb40f600883596ac37f9bfdf43d7d0db5d0278b9
mathêe <91820113+notmathee@users.noreply.github.com>
2022-10-06 19:47:26 -0300

    carrosel feed

--- src/components/Feed/components/TodosTorneios/Torneio/index.js
+++ src/components/Feed/components/TodosTorneios/Torneio/index.js
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import '../../../index.css'
 import placeholderArt from './arteOnda2.png'
+import placeholderArt2 from '../../../assets/CastelolAnimado.jpg'
+import placeholderArt3 from '../../../../Home/assets/CSGO.png'
+
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai'
 
 function TorneioUm() {
+    const carousel = useRef(null)
+    const [cooldown, setCooldown] = useState('')
+
+    const handleLeftClick = e => {
+        setCooldown('disabled')
+        e.preventDefault()
+
+        carousel.current.scrollLeft -= carousel.current.offsetWidth
+        setTimeout(() => {
+            setCooldown('')
+        }, 550);
+    }
+
+    const handleRightClick = e => {
+        e.preventDefault()
+        setCooldown('disabled')
+
+        carousel.current.scrollLeft += carousel.current.offsetWidth
+        setTimeout(() => {
+            setCooldown('')
+        }, 500);
+    }
+
     return (
-        <a href='./participar' className="Torneio" style={{backgroundImage: `url(${placeholderArt})`}}>
-            <h1 className='TorneioH1'>Gaia Cup</h1>
-        </a>
+        <div className='torneioSetasMainContainer'>
+            <div className='torneioContainer' ref={carousel}>
+
+                <a href='./participar' className='Torneio' style={{backgroundImage: `url(${placeholderArt})`}}>
+                    <h1 className='TorneioH1'>Gaia Cup</h1>
+                </a>
+
+                <a href='./participar' className='Torneio' style={{backgroundImage: `url(${placeholderArt2})`}}>
+                    <h1 className='TorneioH1'>Gaia Cup</h1>
+                </a>
+
+                <a href='./participar' className='Torneio' style={{backgroundImage: `url(${placeholderArt3})`}}>
+                    <h1 className='TorneioH1'>Gaia Cup</h1>
+                </a>
+
+            </div>
+            <div className='containerTorneioSetas'>
+                <button className='buttonSeta' onClick={handleLeftClick} disabled={cooldown}><AiOutlineArrowLeft/></button>
+                <button className='buttonSeta' onClick={handleRightClick} disabled={cooldown}><AiOutlineArrowRight/></button>
+            </div>
+        </div>
         )
 }
 
