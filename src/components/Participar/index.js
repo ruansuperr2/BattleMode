34ba3fe7948095701f41444bcd04b827afd059cc
mathêe <91820113+notmathee@users.noreply.github.com>
2022-10-14 17:55:39 -0300

    coloquei abas no participar e tentando usar modal

--- src/components/Participar/index.js
+++ src/components/Participar/index.js
@@ -1,27 +1,84 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
+import ModalCustom from '../Modal';
+import { showModal, closeModal} from "../Modal";
 import TorneioHeader from "./components/TorneioHeader";
 import './index.css';
 
 function Participar() {
+    const [page, setPage] = useState('geral')
+
+    const callModal = () => {
+        showModal('spin', 'Deseja participar do Torneio?', 'Participar')
+        setTimeout(() => {
+            closeModal('success', 'deu certo', 'Participar')
+        }, 2000);
+    }
+
+    useEffect(() => {
+        switch(page){
+            case 'geral':
+                document.querySelector('.divCampoGeral').style.display = 'flex'
+                document.querySelector('.divCampoEquipe').style.display = 'none'
+                document.querySelector('.divCampoParticipar').style.display = 'none'
+
+                document.querySelector('.geral').classList.add('perfilActive')
+                document.querySelector('.equipe').classList.remove('perfilActive')
+                document.querySelector('.torneio').classList.remove('perfilActive')
+                break
+            case 'equipe':
+                document.querySelector('.divCampoGeral').style.display = 'none'
+                document.querySelector('.divCampoEquipe').style.display = 'flex'
+                document.querySelector('.divCampoParticipar').style.display = 'none'
+
+                document.querySelector('.geral').classList.remove('perfilActive')
+                document.querySelector('.equipe').classList.add('perfilActive')
+                document.querySelector('.torneio').classList.remove('perfilActive')
+                break
+            case 'torneio':
+                document.querySelector('.divCampoGeral').style.display = 'none'
+                document.querySelector('.divCampoEquipe').style.display = 'none'
+                document.querySelector('.divCampoParticipar').style.display = 'flex'
+
+                document.querySelector('.geral').classList.remove('perfilActive')
+                document.querySelector('.equipe').classList.remove('perfilActive')
+                document.querySelector('.torneio').classList.add('perfilActive')
+                break
+        }
+    })
+
     return (
         <div className='divParticiparMainContainer'>
-            <Navbar/>
-                <div className="divMainTorneio">
-                    <div className="divHeader">
+            <Navbar page={'usuario'} />
+            <div className="divMainTorneio" />
+
+            <div>
+                <ModalCustom/>
+                <div className='perfilNavigation'>
+                    <div onClick={() => setPage('geral')} className='perfilConfig geral'><div className='imgUsuarioGearEditing'/>Visão Geral</div>
+                    <div onClick={() => setPage('equipe')} className='perfilConfig equipe'><div className='imgUsuarioGearEditing'/>Equipes</div>
+                    <div onClick={() => callModal()} className='perfilConfig torneio'><div className='imgUsuarioGearEditing'/>Torneios</div>
+                    {/* <div onClick={() => setPage('config')} className='perfilConfig config'><div className='imgUsuarioGearEditing'/>Configurar Perfil</div> */}
+                </div>
+                <div className='divCampoGeral campos' >
+                    <div style={{display: 'flex', flexDirection: 'column'}}>
+                        <TorneioHeader/>
                         <TorneioHeader/>
                     </div>
-                    <div className="divTorneioDesc">
-                        <div className="divButtonParticipar">
-                            <button className="buttonParticipar">Participar</button>
-                        </div>
-                        <div className="divDesc">
-                            <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-                        </div>
+                    <div className="divDesc">
+                        <p className="descricao">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
                     </div>
-                    <Footer/>
                 </div>
+                <div className='divCampoEquipe campos' >
+                    <h1 className="TeamsOnProfile"><div className='divImgFundoMainContainer'/>2</h1>
+                </div>
+                <div className='divCampoParticipar campos' >
+                    <h1 className="UserNameOnProfile"><div className='divImgFundoMainContainer'/>3</h1>
+                
+                </div>
+            </div>
+            <Footer/>
         </div>
     )
 }
