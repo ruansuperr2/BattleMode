6ce8a0e943784583cec7e4529adaf88277ccdc07
Markin <91993583+lMarkinl@users.noreply.github.com>
2022-11-01 16:06:07 -0300

    Add files via upload

--- src/components/CriarTorneio/index.js
+++ src/components/CriarTorneio/index.js
@@ -1,28 +1,10 @@
-import React, {useState} from "react";
+import React from "react";
 import './index.css'
 import NavBar from '../Navbar'
 import Footer from '../Footer'
 import Select from 'react-select'
 
-let getGamesTry = 0
 function CriarTorneio (){
-
-   
-    const [jogo, setJogo] = useState([])
-    const callGames = async() => {
-        try{
-            const response = await fetch('http://localhost:3000/api/jogo')
-            const data = response.json()
-            data.then(
-                (val) => {setJogo(val.data)})
-        }catch(error){
-            console.log(error)
-        }
-    }
-    if(getGamesTry < 10){
-        getGamesTry++
-        callGames()
-    }
 
     const chaves = [
         { value: 'classificatoria', label: 'Classificatoria' },
@@ -32,20 +14,11 @@ function CriarTorneio (){
       ]
     
     const jogos = [
-        // { value: 'valorant', label: 'Valorant' },
-        // { value: 'formula1', label: 'Formula 1' },
-        // { value: 'leagueoflegends', label: 'League of Legends' },
-        // { value: 'fifa22', label: 'Fifa 22' }
+        { value: 'valorant', label: 'Valorant' },
+        { value: 'formula1', label: 'Formula 1' },
+        { value: 'leagueoflegends', label: 'League of Legends' },
+        { value: 'fifa22', label: 'Fifa 22' }
       ]
-
-    for(let i = 0; i < jogo.length;i++){
-        // após chamar os jogos no banco de dados, ele puxa o jogo de lá, e da um loop, enviando todos os 
-        // dados dentro do array do banco de dados para o array que será renderizado.
-        jogos.push({
-            value: jogo[i].id,
-            label: jogo[i].nome
-        })
-    }
     
     
     return(
@@ -54,27 +27,37 @@ function CriarTorneio (){
 
             <div className="divMainCriarTorneio paddingLeft">
                 <div className="divSubCriarTorneio">
-                    <div className="divImgThaumbCriarTorneio">
-                        <label>Thumb</label>
-                    </div>
+                    <div className="divDivisorCriarTorneio">
 
-                    <div className="divImgLogoCriarTorneio">
-                        <label>Logo</label>
-                    </div>
+                        <div className="addthumb">
+                            <div className="imgthumb">
 
-                    <div className="divImgFundoCriarTorneio">
-                        <label>Fundo</label>
-                    </div>
+                            </div>
 
-                    <div className="AddImgCriarTorneio">
-                        <button className="AddThaumb">Adicione sua Thumb</button>
-                        <button className="AddLogo">Adicione sua Logo</button>
-                        <button className="AddFundo">Adicione seu Fundo</button>
-                        
-                        <div className="divInfoCriarTorneio">
-                            <input className="NomeCriarTorneio" placeholder="Digite o nome do torneio aqui..."></input>
-                            <textarea className="InfoCriarTorneio" placeholder="Digite a descrição do torneio aqui..."></textarea>
+                            <button className="funçaoThumb">Adicionar thumb</button>
                         </div>
+
+                        <div className="addlogo">
+                            <div className="imglogo">
+
+                            </div>
+
+                            <button className="funçaoLogo">Adicionar thumb</button>
+                        </div>
+
+                        <div className="addfundo">
+                            <div className="imgfundo">
+
+                            </div>
+
+                            <button className="funçaologo">Adicionar thumb</button>
+                        </div>
+
+                    </div>
+
+                    <div className="divInfoCriarTorneio">
+                        <input placeholder="Digite o nome do seu torneio aqui..."></input>
+                        <textarea placeholder="Digite as informações do seu torneio aqui..."></textarea>
                     </div>
                 </div>
 
