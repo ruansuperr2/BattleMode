aa2e670a102a8a97eb85165f95da8077561761f0
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-11-22 14:20:06 -0300

    v0.680d

--- src/components/FindAll/index.js
+++ src/components/FindAll/index.js
@@ -1,6 +1,48 @@
+import React, { useState} from 'react'
+import Footer from '../Footer'
+import './index.css'
+
+let getUsersTry = 0
+
+
 export default function FindAll() {
+
+    const [users, setUsers] = useState([])
+
+    const getUsers = async () => {
+        try{
+            const responseUsers = await fetch('https://web-production-8ce4.up.railway.app/api/user/')
+            const dataUsers = responseUsers.json()
+            dataUsers.then(
+                (val) => {
+                    setUsers(val.data)
+                }
+            )   
+        }catch(error){
+            console.log(error)
+        }
+    }
+    
+    if(getUsersTry < 3){
+        getUsersTry++
+        getUsers()
+    }
+
+    
     return (
         <div className="divContainerFindAll">
+            <div className='organizeList'>
+                <div className='containerSpecificUser paddingLeft'>
+                    {
+                        users.map( (users) => {
+                                return <div onClick={() => {window.location.href = `/u/${users.username}`}} key={users.id} style={{backgroundImage: `url(${users.imgFundo})`}} className='userHighlightedFeed bigTourneamentHiglightOne'>
+                                <label><img src={users.icon} alt='img'/>{users.username}</label>
+                            </div>
+                        })
+                    }
+                </div>
+            </div>
+            <Footer/>
         </div>
     )
 }
