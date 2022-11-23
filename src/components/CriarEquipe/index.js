95aa2835cbea0d23b36030b220ec8003ba737f1d
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-11-23 16:59:09 -0300

    Add files via upload

--- src/components/CriarEquipe/index.js
+++ src/components/CriarEquipe/index.js
@@ -1,12 +1,228 @@
-import react from 'react'
+import React, { useEffect, useState } from 'react'
+import { storage } from '../FireBase';
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import './index.css';
 import Navbar from '../Navbar'
 import Footer from '../Footer'
 import Integrante from './components/Integrante';
 import { AiOutlineCheck, AiOutlinePlus } from 'react-icons/ai'
+import { HiX } from 'react-icons/hi'
+
+let getUsersTry = 0
+let timeObject = {
+    nome: null,
+    tag: null,
+    logo: null,
+    imgFundo: null,
+    equipeAtiva: [],
+    reserva: [],
+    comissaoTecnica: [],
+    jogoPrincipal: 0,
+    conquistas: []
+}
 
 function CriarEquipe() {
+
+    const [loggedUser, setLoggedUser] = useState({})
+    const [users, setUsers] = useState([])
+    const [jogo, setJogo] = useState([])
+    const [torneio, setTorneio] = useState([])
+    const [time, setTime] = useState([])
+    const [name, setName] = useState('')
+    const [tag, setTag] = useState('')
+    const [desc, setDesc] = useState('')
+    const [teamUser, setTeamUser] = useState([])
+
+
+    const [imgUrl, setImgUrl] = useState(null);
+    const [imgUrl2, setImgUrl2] = useState(null);
+    const [imgUrl3, setImgUrl3] = useState(null);
+    const [progresspercent, setProgresspercent] = useState(0);
+    const [progresspercent2, setProgresspercent2] = useState(0);
+    const [progresspercent3, setProgresspercent3] = useState(0);
+
+    const handleSubmit = (e) => {
+      const file = e.target.files[0]
+      if (!file) return;
+      const storageRef = ref(storage, `logo/${file.name}`);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+  
+      uploadTask.on("state_changed",
+        (snapshot) => {
+          const progress =
+            Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+          setProgresspercent(progress);
+        },
+        (error) => {
+          alert(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImgUrl(downloadURL)
+          })
+        }
+      )
+    }
     
+    const handleSubmitImgFundo = (e) => {
+        const file = e.target.files[0]
+        if (!file) return;
+        const storageRef = ref(storage, `ImgFundo/${file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+    
+        uploadTask.on("state_changed",
+          (snapshot) => {
+            const progress =
+              Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+            setProgresspercent2(progress);
+          },
+          (error) => {
+            alert(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              setImgUrl2(downloadURL)
+            })
+          }
+        )
+      }
+
+      const handleSubmitImgFundoDois = (e) => {
+        const file = e.target.files[0]
+        if (!file) return;
+        const storageRef = ref(storage, `ImgFundoDois/${file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+    
+        uploadTask.on("state_changed",
+          (snapshot) => {
+            const progress =
+              Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+            setProgresspercent3(progress);
+          },
+          (error) => {
+            alert(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              setImgUrl3(downloadURL)
+            })
+          }
+        )
+      }
+
+    const callTorneio = async() => {
+        try{
+            const response = await fetch('https://web-production-8ce4.up.railway.app/api/torneio')
+            const data = response.json()
+            data.then(
+                (val) => {setTorneio(val.data)})
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    const callGames = async() => {
+        try{
+            const response = await fetch('https://web-production-8ce4.up.railway.app/api/jogo')
+            const data = response.json()
+            data.then(
+                (val) => {setJogo(val.data)})
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    const callTime = async () => {
+        try{
+            const responseUser = await fetch('https://web-production-8ce4.up.railway.app/api/time/')
+            const dataTime = responseUser.json()
+
+            dataTime.then(
+                (val) => {
+                    setTime(val.data)
+                }
+            )   
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    const getUsers = async () => {
+        try{
+            const responseUser = await fetch('https://web-production-8ce4.up.railway.app/api/user/' + JSON.parse(localStorage.getItem('dasiBoard')))
+            const dataUser = responseUser.json()
+
+            const responseUsers = await fetch('https://web-production-8ce4.up.railway.app/api/user/')
+            const dataUsers = responseUsers.json()
+            dataUsers.then(
+                (val) => {
+                    setUsers(val.data)
+                }
+            )
+            dataUser.then(
+                (val) => {
+                    setLoggedUser(val.data)
+                }
+            )   
+        }catch(error){
+            console.log(error)
+        }
+    }
+    
+    if(getUsersTry < 3){
+        getUsersTry++
+        getUsers()
+        callGames()
+        callTorneio()
+        callTime()
+    }
+
+    const [inputProcurar, setInputProcurar] = useState('')
+    const [jogadores, setJogadores] = useState([])
+
+    const handleChange = e => {
+        setInputProcurar(e.target.value)
+    }
+
+    const addJogador = nomeJogador => {
+        setJogadores([...jogadores, users.find(usuario => {return usuario.username === nomeJogador} )])
+        
+        setInputProcurar('')
+    }
+
+    const handleRemove = id => {
+        const novaListaJogadores = jogadores.filter((item) => item.id !== id)
+        setJogadores(novaListaJogadores)
+    }
+
+    const sendEverything = async() => {
+        try{
+            const requestOptions = {
+                    method: 'POST',
+                    headers: {'Content-type': 'application/json'},
+                    body: JSON.stringify({
+                        nome: name,
+                        tag: tag,
+                        logo: imgUrl,
+                        imgFundo: imgUrl2,
+                        equipeAtiva: JSON.stringify(jogadores.map((item) => {return item['id']})),
+                        reserva: JSON.stringify([]),
+                        comissaoTecnica: JSON.stringify([]),
+                        jogoPrincipal: 0,
+                        conquistas: JSON.stringify([])
+                    })
+                    
+            }
+            await fetch('https://web-production-8ce4.up.railway.app/api/time/', requestOptions)
+            setTimeout(() => {
+                // window.location.href = ' '
+            }, 1000)
+        }catch(error){
+                console.log(error)
+        }
+        console.log(timeObject)
+    }
+    console.log(imgUrl2, imgUrl)
     return (
         <div className='mainContainerCriarEquipe'>
             <div className='divCriarEquipe paddingLeft'>
@@ -15,35 +231,112 @@ function CriarEquipe() {
                     <div className='divDemoCriarEquipe'>
                         <div className='divFlex'>
                             <div className='divImgThumb divImgDemo'>
-                                <label className='labelImgCriarEquipe'>Capa</label>
+                                {/* <img className='gearSelectImage' src={require('./components/assets/selecionar100x100.png')}/> */}
+                                <label className='labelImgCriarEquipe'>
+                                {
+                                    !imgUrl &&
+                                    <div className='outerbar'>
+                                    <div className='innerbar' style={{ width: `100%` }}>Capa {progresspercent2}%</div>
+                                    </div>
+                                }
+                                {
+                                    imgUrl &&
+                                    
+                                    <img src={imgUrl2} alt='uploaded file' className='imgUploadedTeam' style={{borderColor: loggedUser.corP}} />
+                                }
+                                
+                                <form className='form' style={{borderColor: loggedUser.corP}}>
+                                    <input style={{borderColor: loggedUser.corP, display: 'none'}} onChange={(event) => {handleSubmitImgFundo(event); 
+                                        console.log('lao', event)}} className='inputTypeFile' type='file' accept=".png,.jpeg"/> 
+                                </form>
+
+                                </label>
+  
                             </div>
-                            <button className='addThumb addButtonCriarEquipe'><AiOutlinePlus style={{fontSize: '20px', color: '#fc6b03', backgroundColor: 'transparent'}}/></button>
+                                {/* <button className='addThumb addButtonCriarEquipe'><AiOutlinePlus style={{fontSize: '20px', color: '#fc6b03', backgroundColor: 'transparent'}}/></button> */}
                         </div>
 
                         <div className='divFlexLogo'>
                             <div className='divImgLogo divImgDemo'>
-                                <label className='labelImgCriarEquipeLogo'>Logo</label>
+                                <label className='labelImgCriarEquipeLogo'>
+                                {
+                                    !imgUrl &&
+                                    <div className='outerbar'>
+                                    <div className='innerbar' style={{ width: `100%` }}>Logo {progresspercent}%</div>
+                                    </div>
+                                }
+                                {
+                                    imgUrl &&
+                                    
+                                    <img src={imgUrl} alt='uploaded file' className='imgUploadedLogo' style={{borderColor: loggedUser.corP}} />
+                                }
+                                
+                                <form className='form' style={{borderColor: loggedUser.corP}}>
+                                    <input style={{borderColor: loggedUser.corP, display: 'none'}} onChange={(event) => {handleSubmit(event); 
+                                        console.log('lao', event)}} className='inputTypeFile' type='file' accept=".png,.jpeg"/> 
+                                </form>
+
+                                </label>
                             </div>
-                            <button className='addLogo addButtonCriarEquipe'><AiOutlinePlus style={{fontSize: '20px', color: '#fc6b03', backgroundColor: 'transparent'}}/></button>
+                            {/* <button className='addLogo addButtonCriarEquipe'><AiOutlinePlus style={{fontSize: '20px', color: '#fc6b03', backgroundColor: 'transparent'}}/></button> */}
                         </div>
 
                         <div className='divFlex'>
                             <div className='divImgFundo divImgDemo'>
-                                <label className='labelImgCriarEquipe'>Fundo</label>
+                                <label className='labelImgCriarEquipe'>
+                                    {
+                                        !imgUrl &&
+                                        <div className='outerbar'>
+                                        <div className='innerbar' style={{ width: `100%` }}>Fundo {progresspercent3}%</div>
+                                        </div>
+                                    }
+                                    {
+                                        imgUrl &&
+                                        
+                                        <img src={imgUrl3} alt='uploaded file' className='imgUploadedLogo' style={{borderColor: loggedUser.corP}} />
+                                    }
+                                    
+                                    <form className='form' style={{borderColor: loggedUser.corP}}>
+                                        <input style={{borderColor: loggedUser.corP, display: 'none'}} onChange={(event) => {handleSubmitImgFundoDois(event); 
+                                            console.log('lao', event)}} className='inputTypeFile' type='file' accept=".png,.jpeg"/> 
+                                    </form>
+                                </label>
                             </div>
-                            <button className='addFundo addButtonCriarEquipe'><AiOutlinePlus style={{fontSize: '20px', color: '#fc6b03', backgroundColor: 'transparent'}}/></button>
+                            {/* <button className='addFundo addButtonCriarEquipe'><AiOutlinePlus style={{fontSize: '20px', color: '#fc6b03', backgroundColor: 'transparent'}}/></button> */}
                         </div>
                     </div>
 
                     <div className='infoContainer'>
                         <div className='divDescCriarEquipe'>
-                            <input className='inputProcurarJogador' placeholder='Nome da equipe...'/>
-                            <textarea className='textareaEquipeDescricao' placeholder='Descrição da equipe...'/>
-                            <button className='buttonConfirmarCriarEquipe'>Confirmar <AiOutlineCheck style={{fontSize: '25px', marginLeft: '.5rem', color: '#fc6b03', backgroundColor: 'transparent'}}/></button>
+                            <input value={name} onChange={event => {setName(event.target.value)}}  className='inputProcurarJogador' placeholder='Nome da equipe...'/>
+                            <input value={tag} style={{marginTop: '10px'}} onChange={event => {setTag(event.target.value)}}  className='inputProcurarJogador' placeholder='Tag da equipe...'/>
+                            <textarea value={desc} onChange={event => {setDesc(event.target.value)}}  className='textareaEquipeDescricao' placeholder='Descrição da equipe...'/>
+                            <button className='buttonConfirmarCriarEquipe' onClick={() => sendEverything()}>Confirmar <AiOutlineCheck style={{fontSize: '25px', marginLeft: '.5rem', color: '#fc6b03', backgroundColor: 'transparent'}}/></button>
                         </div>
 
                         <div className='divInfoCriarEquipe'>
-                            <Integrante/>
+            
+                        <div className='inputButtonFlex'>
+                            <input 
+                                className='inputProcurarJogador' 
+                                placeholder='Adicionar jogador...'
+                                onChange={handleChange}
+                                value={inputProcurar}/>
+                            <button 
+                                className='addJogador addButtonCriarEquipe'
+                                onClick={() => addJogador(inputProcurar)}><AiOutlinePlus style={{fontSize: '20px', color: '#fc6b03', backgroundColor: 'transparent'}}/></button>
+                        </div>
+                        <div className='divMostrarJogadores'>
+                                {jogadores.map((item) => (
+                                    <div className='cardBody' key={item.id}>
+                                        <div className='userIcon' style={{backgroundPosition: 'center', backgroundSize: 'cover',backgroundImage: `url(${item.icon})`}}/>
+                                        <span className='userName'><a href='#'>{item.username}</a></span>
+                                        <button 
+                                            className='buttonRemoveJogador'
+                                            onClick={() => handleRemove(item.id)}><HiX style={{fontSize: '20px', color: '#fc6b03'}}/></button>
+                                    </div>
+                                ))}
+                        </div>
                         </div>
                     </div>
                 </div>
